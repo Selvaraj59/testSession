a714aa0323a344402b0c2e3330590154fd9498bd
Selvaraj59 <rajabvijay@gmail.com>
2017-11-26 11:15:06 +0530

    Adding UserName Validator for security

---
+++ Session/Session/UsernameValidator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IdentityModel.Selectors;
+
+namespace Microsoft.Samples.Session
+{
+    public class UsernameValidator : UserNamePasswordValidator
+    {
+        public override void Validate(string userName, string password)
+        {
+
+        }
+    }
+}
